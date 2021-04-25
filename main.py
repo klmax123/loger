82c49a3b68952c1bfda5eca00261af2efcefc529
student.ik <Miss.vestik@yandex.ru>
2021-04-25 14:15:08 +0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import sqlite3
+app = Flask(__name__)
+
+@app.route("/insert")
+def main():
+    connect = sqlite3.connect("db.db")
+    cursor = connect.cursor()
+    cursor.execute("INSERT INTO Loger (type, message, date, time, url) VALUES (?, ?, ?, ?, ?)", (1, "test", "2020-12-05", "11:11:11", "/ures/add"))
+    connect.commit()
+    return ""
+
+
+
+
+
+app.run()
